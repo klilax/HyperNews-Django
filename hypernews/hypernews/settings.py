a97fba764b4ff4725ccd096dc9974a9eb7d9c40b
Khalil Ahmed <klilax@hotmail.co.uk>
2022-02-28 21:49:59 +0300

    news serving the news

--- hypernews/hypernews/settings.py
+++ hypernews/hypernews/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+NEWS_JSON_PATH = 'hypernews/news.json'  # You can choose any path you want
+NEWS_JSON_PATH = os.environ.get('NEWS_JSON_PATH') or NEWS_JSON_PATH  # DO NOT MODIFY THIS LINE
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news',
 ]
 
 MIDDLEWARE = [
--- hypernews/news/views.py
+++ hypernews/news/views.py
@@ -1,4 +1,6 @@
+import json
 from django.shortcuts import render
+from django.conf import settings
 from django.http.response import HttpResponse
 from django.views import View
 
@@ -7,3 +9,19 @@ class Welcome(View):
 
     def get(self, request):
         return HttpResponse('Coming Soon')
+
+
+class ServeNews(View):
+    template_name = 'news.html'
+
+    def get(self, request, **kwargs):
+        with open(settings.NEWS_JSON_PATH, 'r', encoding='utf-8') as json_file:
+            news_repository = json.load(json_file)
+            link = kwargs['link']
+
+            render_news = {}
+            for news in news_repository:
+                if news['link'] == int(link):
+                    render_news = news
+                    break
+            return render(request, self.template_name, {'news': render_news})
